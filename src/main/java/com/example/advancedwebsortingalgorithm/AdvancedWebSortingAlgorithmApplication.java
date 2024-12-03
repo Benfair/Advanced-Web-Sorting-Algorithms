13c444dd450f90597cef6ae17fa00fbe8a614d53
Benfair <benedicta.asare@amalitech.com>
2024-12-03 08:26:27 +0000

    Create new project

---
+++ src/main/java/com/example/advancedwebsortingalgorithm/AdvancedWebSortingAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.example.advancedwebsortingalgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedWebSortingAlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedWebSortingAlgorithmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/advancedwebsortingalgorithm/AdvancedWebSortingAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advancedwebsortingalgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedWebSortingAlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
